2739e17489a976d9a14da851cb37749c6a169d67
Luiz Henrique Murakami <luizhm.murakami@gmail.com>
2019-05-18 20:49:44 -0300

    Added Files
    Geany - JAVA

---
+++ Rede de Locadoras de Veículos/Cliente.java
@@ -0,0 +1,16 @@
+public class Cliente extends Pessoa
+{
+	private int CNH;
+	private String ValidadeCNH;
+	
+	public Cliente(String n)
+	{
+		super(n);
+	}
+	
+	public void setCNH(int c) { CNH = c; }
+	public void setValidadeCNH(String v) { ValidadeCNH = v; }
+	
+	public int getCNH() { return CNH; }
+	public String getValidadeCNH() { return ValidadeCNH; }
+}
---
+++ Rede de Locadoras de Veículos/Tela.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+public class Tela{
+	
+	public static void limpar(){
+		try{
+			ProcessBuilder pb =
+				new ProcessBuilder("cmd", "/c", "cls");
+				
+			pb.inheritIO();
+			pb.start();
+		}catch(IOException e){
+			e.printStackTrace();
+		}		
+	}
+}
